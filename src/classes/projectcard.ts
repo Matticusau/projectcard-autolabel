9089c4c67ccc68c2abaf3c8cd560cbc9f853dde6
Matticusau <matt.lavery@outlook.com>
2021-02-07 23:57:12 +1000

    label logic

--- src/classes/projectcard.ts
+++ src/classes/projectcard.ts
@@ -11,12 +11,89 @@ import { CoreModule, GitHubModule, Context, PullRequestPayload } from '../types'
 import { IssueLabels } from './index';
 import { PullsGetResponseData } from '@octokit/types/dist-types'
 import { ProjectsGetCardResponseData } from '@octokit/types/dist-types'
-interface DeleteBranchConfig {
-    deny?: string[];
-    allow?: string[];
+interface AutoLabelConfig {
+    column: string;
+    add_labels?: string[];
+    remove_labels?: string[];
 }
+// autolabel-config: '[{"column":"in-progress"; "add_labels":["in-progress"]; "remove_labels":["triage"]}]'
 
 export class ProjectCardClass {
 
+    // properties
+    private core: CoreModule;
+    private github: GitHubModule;
+    private autoLabelConfig: AutoLabelConfig[];
+    public labelsToAdd: string[] | undefined;
+    public labelsToRemove: string[] | undefined;
+
+    constructor(core: CoreModule, github: GitHubModule) {
+        this.core = core;
+        this.github = github;
+        this.autoLabelConfig = [];
+        this.labelsToAdd = [];
+        this.labelsToRemove = [];
+    }
+
+    private matchConfigFromActionInputYaml(json: string) : AutoLabelConfig[] {
+        try{
+          // convert json to string array
+          this.core.debug('json: ' + json);
+          //let pattern : string[] = JSON.parse(json);
+          let pattern : AutoLabelConfig[] = JSON.parse(json);
+          this.core.debug('json pattern: ' + JSON.stringify(pattern));
+  
+          return pattern;
+        } catch (error) {
+          this.core.setFailed(error.message);
+          throw error;
+        }
+    }
+
+    async GetProjectColumnAutoLabelConfig() : Promise<boolean> {
+        try {
+            this.core.debug('>> GetProjectColumnAutoLabelConfig()');
+            const autoLabelConfig : AutoLabelConfig[] = this.matchConfigFromActionInputYaml(this.core.getInput('autolabel-config'));
+            
+            this.autoLabelConfig = autoLabelConfig;
+            return true;
+        } catch (error) {
+            this.core.setFailed(error.message);
+            throw error;
+        }
+    }
+
+    async columnHasAutoLabelConfig(columnName: string) : Promise<boolean> {
+        try {
+            let blnResponse: boolean = false;
+            if (undefined !== this.autoLabelConfig && this.autoLabelConfig.length > 0) {
+                for(let iConfig = 0; iConfig < this.autoLabelConfig.length; iConfig++) {
+                    if (this.autoLabelConfig[iConfig].column = columnName) {
+                        blnResponse = true;
+                    }
+                }
+            }
+            return blnResponse;
+        } catch (error) {
+            this.core.setFailed(error.message);
+            throw error;
+        }
+    }
+
+    async GetLabelChangesToMake(columnName: string) : Promise<void> {
+        try {
+            if (undefined !== this.autoLabelConfig && this.autoLabelConfig.length > 0) {
+                for(let iConfig = 0; iConfig < this.autoLabelConfig.length; iConfig++) {
+                    if (this.autoLabelConfig[iConfig].column = columnName) {
+                        this.labelsToAdd = this.autoLabelConfig[iConfig].add_labels;
+                        this.labelsToRemove = this.autoLabelConfig[iConfig].remove_labels;
+                    }
+                }
+            }
+        } catch (error) {
+            this.core.setFailed(error.message);
+            throw error;
+        }
+    }
 
 }
--- src/projectcardmovehandler.ts
+++ src/projectcardmovehandler.ts
@@ -17,29 +17,89 @@ export default async function projectCardMoveHandler(core: CoreModule, github: G
     try {
         core.info('context: ' + JSON.stringify(github.context));
 
+        const projectCardClass = new ProjectCardClass(core, github);
+
+        // get the config
+        await projectCardClass.GetProjectColumnAutoLabelConfig();
+
         var projectCardWebhookPayload: ProjectCardWebhookPayload = github.context.payload;
         // make sure this is an issue and not a note
         if (undefined !== projectCardWebhookPayload.project_card && undefined !== projectCardWebhookPayload.project_card.content_url) {
             core.info('content_url is defined');
             core.info('column_id: ' + projectCardWebhookPayload.project_card.column_id.toString());
             
-            // get the issue number
-            let issueContentUrl: string = projectCardWebhookPayload.project_card.content_url;
-            let issueNumber: number = 0;
-            if (issueContentUrl.indexOf('/issues/') > 0) {
-                issueContentUrl = issueContentUrl.substring(issueContentUrl.indexOf('/issues/') + 8)
-                core.info('issueContentUrl: ' + issueContentUrl);
-                issueNumber = parseInt(issueContentUrl);
-            }            
-
-            // get the issue details
+            // octokit
             const myToken = core.getInput('repo-token');
             const octokit = github.getOctokit(myToken);
-            const { data: issueResponseData } = await octokit.issues.get({
+            
+            // get the column details
+            const { data: columnResponseData } = await octokit.projects.getColumn({
                 ...github.context.repo,
-                issue_number: issueNumber,
+                column_id: projectCardWebhookPayload.project_card.column_id
             });
-            core.info('issueResponseData: ' + JSON.stringify(issueResponseData));
+            core.info('columnResponseData: ' + JSON.stringify(columnResponseData));
+            core.info('column name: ' + columnResponseData.name);
+
+            // make sure the column matches one of our rules
+            if (await projectCardClass.columnHasAutoLabelConfig(columnResponseData.name)) {
+                core.info('rule found for column');
+
+                await projectCardClass.GetLabelChangesToMake(columnResponseData.name);
+
+                // get the issue number
+                let issueContentUrl: string = projectCardWebhookPayload.project_card.content_url;
+                let issueNumber: number = 0;
+                if (issueContentUrl.indexOf('/issues/') > 0) {
+                    issueContentUrl = issueContentUrl.substring(issueContentUrl.indexOf('/issues/') + 8)
+                    core.info('issueContentUrl: ' + issueContentUrl);
+                    issueNumber = parseInt(issueContentUrl);
+                }
+
+                // get the issue details
+                const { data: issueResponseData } = await octokit.issues.get({
+                    ...github.context.repo,
+                    issue_number: issueNumber,
+                });
+                core.info('issueResponseData: ' + JSON.stringify(issueResponseData));
+
+                // get the issue labels
+                const { data: issueLabelsData } = await octokit.issues.listLabelsOnIssue({
+                    ...github.context.repo,
+                    issue_number: issueNumber,
+                });
+
+                if (!issueLabelsData) {
+                    core.error('Could not get pull request labels, exiting');
+                    return;
+                }
+
+                var issueLabels = new IssueLabels(issueLabelsData);
+
+                // update the labels
+                if (undefined !== projectCardClass.labelsToRemove && projectCardClass.labelsToRemove.length > 0) {
+                    core.info('labelsToRemove: ' + JSON.stringify(projectCardClass.labelsToRemove));
+                    for(let iLabel = 0; iLabel < projectCardClass.labelsToRemove.length; iLabel++) {
+                        issueLabels.removeLabel(projectCardClass.labelsToRemove[iLabel]);
+                    }
+                }
+                if (undefined !== projectCardClass.labelsToAdd && projectCardClass.labelsToAdd.length > 0) {
+                    core.info('labelsToAdd: ' + JSON.stringify(projectCardClass.labelsToAdd));
+                    for(let iLabel = 0; iLabel < projectCardClass.labelsToAdd.length; iLabel++) {
+                        issueLabels.addLabel(projectCardClass.labelsToAdd[iLabel]);
+                    }
+                }
+
+                if (issueLabels.haschanges) {
+                            
+                    // set the label
+                    await octokit.issues.setLabels({
+                        owner: github.context.repo.owner,
+                        repo: github.context.repo.repo,
+                        issue_number: issueNumber,
+                        labels: issueLabels.labels
+                    });
+                }
+            }
 
         }
 
